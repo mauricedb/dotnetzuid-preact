2b186d23382c1a4a810783e825005c84e7a3c34a
Maurice de Beijer <maurice.de.beijer@gmail.com>
2017-09-12 09:59:57 +0200

    Use the IntersectionObserver to load movies when scrolling

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
+// import 'intersection-observer'
 import App from "./components/app";
 
 export default App;
--- src/routes/movies/index.js
+++ src/routes/movies/index.js
@@ -1,24 +1,58 @@
 import { h, Component } from "preact";
 
 import Movie from "../../components/movie";
+import style from "./style";
 
 class Movies extends Component {
   state = {
-    movies: []
+    movies: [],
+    take: 5
+  };
+
+  constructor() {
+    super();
+
+    this.observer = new IntersectionObserver(this.onIntersection);
+    this.observer.POLL_INTERVAL = 100; // Time in milliseconds for the polyfill.
+  }
+
+  onIntersection = entries => {
+    const entry = entries[0];
+    if (
+      entry &&
+      entry.isIntersecting &&
+      this.state.take < this.state.movies.length
+    ) {
+      this.setState(state => ({ take: state.take + 5 }));
+    }
   };
 
   componentDidMount() {
     fetch("/api/movies.json")
       .then(rsp => rsp.json())
-      .then(movies => this.setState(() => ({ movies })));
+      .then(movies => this.setState(() => ({ movies })))
+      .then(() => {
+        this.observer.observe(this.bottom);
+      });
   }
 
-  render({}, { movies }) {
+  componentWillUnmount() {
+    this.observer.unobserve(this.bottom);
+  }
+
+  render({}, { movies, take }) {
     return (
       <div>
         <h1>Movies</h1>
 
-        <div>{movies.map(movie => <Movie movie={movie} />)}</div>
+        <div>{movies.slice(0, take).map(movie => <Movie movie={movie} />)}</div>
+
+        <div
+          class={style.bottom}
+          ref={el => {
+            this.bottom = el;
+          }}
+        />
       </div>
     );
   }
