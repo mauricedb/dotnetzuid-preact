768a9eb60163be14004aa21b03fa4cbca89b7e33
Maurice de Beijer <maurice.de.beijer@gmail.com>
2017-09-21 11:21:04 +0200

    xs is sm in BS4

--- src/components/genre/index.js
+++ src/components/genre/index.js
@@ -4,7 +4,7 @@ import style from "./style";
 
 const Genre = ({ genre }) => {
   return (
-    <button class={classnames("btn", "btn-info", "btn-xs", style.btnGenre)}>
+    <button class={classnames("btn", "btn-info", "btn-sm", style.btnGenre)}>
       {genre}
     </button>
   );
